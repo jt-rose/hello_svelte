db5d55acd74779f5b37095895bb4fc066a6919af
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-10-08 13:10:09 -0400

    add clicker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    let name = "Jeff"
+    let clicked = 0;
+    const addOnClick = () => {
+        clicked++
+    }
+
+    const minusOnClick = () => {
+        clicked--
+    }
+</script>
+
+<h1>Welcome {name}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Clicked {clicked} times</p>
+<button on:click={minusOnClick}>-</button>
+<button on:click={addOnClick}>+</button>
