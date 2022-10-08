97af8f4e50a1771ab8fd17c340fe7a576cdc85d3
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-10-08 13:22:17 -0400

    refactor clicker to new component

---
+++ src/components/clicker.svelte
@@ -0,0 +1,14 @@
+<script>
+    let clicked = 0;
+    const addOnClick = () => {
+        clicked++
+    }
+
+    const minusOnClick = () => {
+        clicked--
+    }
+</script>
+
+<p>Clicked {clicked} times</p>
+<button on:click={minusOnClick}>-</button>
+<button on:click={addOnClick}>+</button>
