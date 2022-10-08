f858c9fde38e54b8951314ebefec018b8f125e60
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-10-08 13:22:25 -0400

    add cats component

---
+++ src/components/cats.svelte
@@ -0,0 +1,10 @@
+<script>
+    let cats = ["Meowgli", "Purrkins", "MoMo"]
+</script>
+
+<h2>Cats</h2>
+<ul>
+{#each cats as cat}
+<li>{cat}</li>
+{/each}
+</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,10 @@
 <script>
+    import Clicker from "../components/clicker.svelte"
+    import Cats from "../components/cats.svelte"
     let name = "Jeff"
-    let clicked = 0;
-    const addOnClick = () => {
-        clicked++
-    }
-
-    const minusOnClick = () => {
-        clicked--
-    }
 </script>
 
 <h1>Welcome {name}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>Clicked {clicked} times</p>
-<button on:click={minusOnClick}>-</button>
-<button on:click={addOnClick}>+</button>
+<Clicker />
+<Cats />
