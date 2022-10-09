a682b9adfa6585ab3144ea973c7aca4c6f0d0898
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-10-08 23:49:25 -0400

    set up function to add cats

--- src/components/cats.svelte
+++ src/components/cats.svelte
@@ -1,5 +1,18 @@
-<script>
+<script lang="ts">
     let cats = ["Meowgli", "Purrkins", "MoMo"]
+
+    const addCat = (e: any) => {
+        if (!e.target) return
+        const formData = new FormData(e.target);
+
+    const data: any = {};
+    for (let field of formData) {
+      const [key, value] = field;
+      data[key] = value;
+    }
+    cats.push(data.name)
+    cats = cats
+    }
 </script>
 
 <h2>Cats</h2>
@@ -8,3 +21,9 @@
 <li>{cat}</li>
 {/each}
 </ul>
+
+<form on:submit|preventDefault={addCat}>
+    <label for="name">Cat Name</label>
+    <input type="text" name="name" id="cat-name" value=""/>
+    <button type="submit" >Add a Cat</button>
+</form>
