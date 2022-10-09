e4b0fe5dc1695cc113071f08f9f084f3d89094ef
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-10-09 00:06:20 -0400

    add editing and deleting of cats

--- src/components/cats.svelte
+++ src/components/cats.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
     let cats = ["Meowgli", "Purrkins", "MoMo"]
+    let editing = ""
+    const setEditing = (target: string) => {
+        editing = target
+    }
 
     const addCat = (e: any) => {
         if (!e.target) return
@@ -13,12 +17,36 @@
     cats.push(data.name)
     cats = cats
     }
+
+    const adoptCat = (name: string) => {
+        cats = cats.filter(n => n !== name)
+    }
+
+    const updateCat = (oldName: string) => (e: any) => {
+        const formData = new FormData(e.target);
+
+    const data: any = {};
+    for (let field of formData) {
+      const [key, value] = field;
+      data[key] = value;
+    }
+
+        cats = cats.map(c => c === oldName ? data.update : c)
+    }
 </script>
 
 <h2>Cats</h2>
 <ul>
 {#each cats as cat}
-<li>{cat}</li>
+{#if cat === editing}
+<form on:submit|preventDefault={updateCat(cat)}>
+    <label for="name">New Name</label>
+    <input type="text" name="update" id="cat-name-edit" value={cat}/>
+    <button type="submit" >Change Name</button>
+</form>
+{:else}
+<li>{cat}</li> <div on:click={() => setEditing(cat)}>Update</div><div on:click={() => adoptCat(cat)}>X</div>
+{/if}
 {/each}
 </ul>
 
